4d6b5997af41799fd7a3877ffadce0d2232e5585
Vanderson <vanderson@Vandersons-iMac.local>
2022-10-26 23:13:20 -0300

    Created the header section (Non-responsive).

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-alert('Js is working!');
+/*-----------------*/
+/*  HEADER-TEXTS   */
+/*-----------------*/
+
+    /*Variables*/
+    let mainText = document.querySelector('.header__content h1');
+    let currentText = 0;
+
+
+    /*Ant-Errors*/
+    if(typeof delay == "undefined")
+        var delay = 100000;
+
+    if(typeof texts == "undefined")
+        var texts = ['Desenvolvedor Web'];
+
+
+    mainText.innerText=texts[currentText].toUpperCase();
+
+    /*Change Texts*/
+    setInterval(()=>{
+        if(currentText < texts.length-1){
+            currentText++;
+        }else {
+            currentText = 0;
+        }
+        
+        mainText.innerText=texts[currentText].toUpperCase();
+    }, delay);
